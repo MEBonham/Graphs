a8dd93e2f29c454ded1020f79dca6657f7dafeea
MEBonham <programmer@mckaybonham.com>
2019-03-04 14:03:10 -0700

    Implements Part 1 (Graph class)

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,18 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, v_id):
+        if v_id in self.vertices:
+            raise IndexError("That vertex already exists")
+        else:
+            self.vertices[v_id] = set()
+    
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("At least one of those vertices does not exist")
+    
